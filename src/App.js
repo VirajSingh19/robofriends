df0ab3abb494e33f821566c9ef58d02999871543
Viraj Singh <virajsingh19@yahoo.com>
2018-07-03 00:55:08 +0530

    added json data to cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {robots} from './Robot';
+// import {robots} from './Robot';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css'
@@ -12,9 +12,16 @@ class App extends Component{
     constructor(){
         super();
         this.state={
-            robots : robots,
+            robots : [],
             searchfield:''
         }
+    }
+
+    componentDidMount()
+    {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response => {return Response.json();})
+        .then(users=>{this.setState({robots:users});});
     }
 
     onSearchChange = (event) =>{
@@ -28,7 +35,7 @@ class App extends Component{
         })    
         return(
              <div className='tc'>
-            <h1>Robo friends</h1>
+            <h1 className='f2'>Robo friends</h1>
             <SearchBox searchChange={this.onSearchChange} />
             <CardList robots={filteredRobots} />
             </div>);
